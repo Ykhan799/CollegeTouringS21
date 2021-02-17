a175c3967e94329e4e09e9c52c02d58f22ed2afd
Ethan <15167713+gomihiko@users.noreply.github.com>
2021-02-17 14:08:44 -0800

    Add placeholder comments

--- CollegeTouring/databaseviewform.cpp
+++ CollegeTouring/databaseviewform.cpp
@@ -7,9 +7,17 @@ databaseViewForm::databaseViewForm(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->distTableWidget->setColumnCount(2);
-    ui->distTableWidget->setRowCount(1);
+    ui->distTableWidget->setHorizontalHeaderLabels(QStringList{"Campus", "Distance"});
     ui->distTableWidget->setColumnWidth(0, 289);
     ui->distTableWidget->setColumnWidth(1, 289);
+
+    ui->distTableWidget->setRowCount(1); // temp
+
+    // populate combo box of colleges
+    // for(...)
+    // ui->collegeSelectBox->addItem(...)
+
+
 }
 
 databaseViewForm::~databaseViewForm()
@@ -19,6 +27,21 @@ databaseViewForm::~databaseViewForm()
 
 void databaseViewForm::on_displayDistButton_clicked()
 {
+    // first clear the table
+    ui->distTableWidget->clearContents();
+
+    // temp
     ui->distTableWidget->setItem(0, 0, new QTableWidgetItem(QString("Hello")));
     ui->distTableWidget->setItem(0, 1, new QTableWidgetItem(QString("World")));
+
+    // read from SQLite db here
+
+    // fetch the number of rows needed to display the data and create the rows
+    // ui->distTableWidget->setRowCount(1);
+
+    // loop over the distances and select the distances from saddleback college and add them to an array/vector
+    // sort the data
+
+    // add to the table
+
 }
