2fd1c4247cdd2c6ab8ca1fe51c0732dff85598ab
Ykhan799 <78000116+Ykhan799@users.noreply.github.com>
2021-03-14 21:41:12 -0700

    Copied wrong file initially

--- CollegeTouring/initialfromuciform.h
+++ CollegeTouring/initialfromuciform.h
@@ -1,59 +1,90 @@
-#ifndef DATABASEVIEWFORM_H
-#define DATABASEVIEWFORM_H
+#ifndef INITIALFROMUCIFORM_H
+#define INITIALFROMUCIFORM_H
 
 #include <QDialog>
-#include <QDebug>
-#include <QSqlDatabase>
-#include <QMessageBox>
-#include <QPixmap>
-#include <vector>
-#include "dbmanager.h"
-#include "modifysouvenirs.h"
-#include "shoppingcart.h"
+#include <queue>
+#include <list>
+#include <dbmanager.h>
 
 namespace Ui {
-class databaseViewForm;
+class initialFromUciForm;
 }
 
-class databaseViewForm : public QDialog
+class initialFromUciForm : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit databaseViewForm(QWidget *parent = nullptr, bool adminUser = false);
-    ~databaseViewForm();
-    std::vector<QString> getActiveTableRow();
+
+    explicit initialFromUciForm(QWidget *parent = nullptr);
+
+    ~initialFromUciForm();
+
+    //!
+    //! \brief createRoute
+    //! recursively finds the campus that is closest to currentCampus, storing it into the route queue
+    //! \param holds the name of the current campus
+    //!
+    void createRoute(QString campus);
 
 private slots:
-    //!
-    //! \brief on_displayDistButton_clicked
-    //! populates the table with distances when the button is clicked
-    //!
-    void on_displayDistButton_clicked();
 
     //!
-    //! \brief on_displaySouvButton_clicked
-    //! populates the table with souvenirs when the button is clicked
-    void on_displaySouvButton_clicked();
+    //! \brief on_cancelTripButton_clicked
+    //! closes the trip planning window when the button is clicked
+    //!
+    void on_cancelTripButton_clicked();
 
     //!
-    //! \brief on_pushButton_clicked
-    //! opens the modify database menu ONLY if user is an administrator.
-    void on_pushButton_clicked();
-    
-    // TODO needs documentation
-    void on_showCartButton_clicked();
+    //! \brief on_beginTripButton_clicked
+    //! begins the trip, calculating the route and changing the display of the window to allow moving through each campus
+    //!
+    void on_beginTripButton_clicked();
+
+    //!
+    //! \brief on_exitButton_clicked
+    //! closes the trip window when the button is clicked
+    //!
+    void on_exitButton_clicked();
+
+    //!
+    //! \brief on_nextButton_clicked
+    //! changes window display to show information about the current campus in the route
+    //!
+    void on_nextButton_clicked();
 
 private:
-    Ui::databaseViewForm *ui;
+    Ui::initialFromUciForm *ui;
 
+    //!
+    //! \brief database used to access data stored in the database
+    //!
     DbManager* database;
-    modifySouvenirs* modDialog;
 
-    bool isAdmin;
+    //!
+    //! \brief initialCampus holds the name of the first campus in the trip
+    //!
+    QString initialCampus;
 
-    shoppingcart *cart;
+    //!
+    //! \brief finalCampus holds the name of the last campus in the trip
+    //!
+    QString finalCampus;
 
+    //!
+    //! \brief campusesToVisit holds all campuses that are planned to be visited in the current trip
+    //!
+    vector<QString> campusesToVisit;
+
+    //!
+    //! \brief route holds the campuses that are visited in the trip in order
+    //!
+    queue<QString> route;
+
+    //!
+    //! \brief totalDistance holds the total distance traveled in the entire trip
+    //!
+    double totalDistance;
 };
 
-#endif // DATABASEVIEWFORM_H
+#endif // INITIALFROMUCIFORM_H
