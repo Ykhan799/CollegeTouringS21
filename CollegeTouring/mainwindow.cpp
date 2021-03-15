dfaacb2b5f7c65e149e5018329b90e2fb93d4c57
Ykhan799 <78000116+Ykhan799@users.noreply.github.com>
2021-03-14 21:35:13 -0700

    Update file to main file

--- CollegeTouring/mainwindow.cpp
+++ CollegeTouring/mainwindow.cpp
@@ -18,20 +18,17 @@ MainWindow::~MainWindow()
 }
 
 /*************************************************************************
- * void on_login_triggered()
+ * void on_actionLog_In_triggered()
  * -----------------------------------------------------------------------
- * This function checks if an input password is valid when the log in
- * button is pressed. If the password is valid, administrator privileges
- * will be granted to the user and the button will afterwards function as a
- * logout button. Otherwise, a message box will be output alerting the user
- * that the password is incorrect and no other changes will be made. While
- * the user is an administrator, clicking the button will remove
- * administrator privileges and require the password to be entered again to
- * restore them.
+ * Creates a window which prompts the user for a username and password. If
+ * entered correctly, the user will be granted admin privileges for the
+ * remainder of the program execution unless the user chooses to log out.
  * -----------------------------------------------------------------------
+ * Username: Admin
+ *
  * Valid passwords:
  * AYEN
- * admin
+ * password
  ************************************************************************/
 void MainWindow::on_actionLog_In_triggered()
 {
@@ -42,6 +39,15 @@ void MainWindow::on_actionLog_In_triggered()
     delete lDialog;
 }
 
+
+/*************************************************************************
+ * void on_actionLog_Out_triggered()
+ * -----------------------------------------------------------------------
+ * Creates a window which notifies the user they have been logged out. The
+ * user is no longer considered an admin for the remainder of the program
+ * execution unless the user successfully attempts to log in.
+ ************************************************************************/
+
 void MainWindow::on_actionLog_Out_triggered()
 {
     isAdmin = false;
@@ -50,8 +56,32 @@ void MainWindow::on_actionLog_Out_triggered()
 
 void MainWindow::on_actionView_Database_triggered()
 {
-    dbView = new databaseViewForm();
+    dbView = new databaseViewForm(nullptr, isAdmin);
     dbView->exec();
     delete dbView;
     dbView = nullptr;
+}
+
+/*************************************************************************
+ * void on_actionVisit_local_colleges_UCI_triggered()
+ * -----------------------------------------------------------------------
+ * Creates a window to handle the planning and execution of a trip. This
+ * trip begins at UCI and visits every closest campus to the current
+ * campus until all colleges have been visited. There is no options for
+ * the user to select their starting point. All initial 11 colleges are
+ * visited.
+ ************************************************************************/
+
+void MainWindow::on_actionVisit_local_colleges_UCI_triggered()
+{
+    initialFromUci = new initialFromUciForm();
+    initialFromUci->exec();
+    delete initialFromUci;
+}
+
+void MainWindow::on_uciButton_clicked()
+{
+    initialFromUci = new initialFromUciForm();
+    initialFromUci->exec();
+    delete initialFromUci;
 }
