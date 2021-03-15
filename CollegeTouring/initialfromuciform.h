d03014c726fe5b0b0f181f44855efe4e1e4655ae
Ykhan799 <78000116+Ykhan799@users.noreply.github.com>
2021-03-14 21:40:17 -0700

    Matches the main file

---
+++ CollegeTouring/initialfromuciform.h
@@ -0,0 +1,59 @@
+#ifndef DATABASEVIEWFORM_H
+#define DATABASEVIEWFORM_H
+
+#include <QDialog>
+#include <QDebug>
+#include <QSqlDatabase>
+#include <QMessageBox>
+#include <QPixmap>
+#include <vector>
+#include "dbmanager.h"
+#include "modifysouvenirs.h"
+#include "shoppingcart.h"
+
+namespace Ui {
+class databaseViewForm;
+}
+
+class databaseViewForm : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit databaseViewForm(QWidget *parent = nullptr, bool adminUser = false);
+    ~databaseViewForm();
+    std::vector<QString> getActiveTableRow();
+
+private slots:
+    //!
+    //! \brief on_displayDistButton_clicked
+    //! populates the table with distances when the button is clicked
+    //!
+    void on_displayDistButton_clicked();
+
+    //!
+    //! \brief on_displaySouvButton_clicked
+    //! populates the table with souvenirs when the button is clicked
+    void on_displaySouvButton_clicked();
+
+    //!
+    //! \brief on_pushButton_clicked
+    //! opens the modify database menu ONLY if user is an administrator.
+    void on_pushButton_clicked();
+    
+    // TODO needs documentation
+    void on_showCartButton_clicked();
+
+private:
+    Ui::databaseViewForm *ui;
+
+    DbManager* database;
+    modifySouvenirs* modDialog;
+
+    bool isAdmin;
+
+    shoppingcart *cart;
+
+};
+
+#endif // DATABASEVIEWFORM_H
