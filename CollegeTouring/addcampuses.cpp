a7955b96b74d3fa43e207e8d1767d345508c8b3e
Ykhan799 <78000116+Ykhan799@users.noreply.github.com>
2021-03-15 17:06:55 -0700

    Fixes bug

--- CollegeTouring/addcampuses.cpp
+++ CollegeTouring/addcampuses.cpp
@@ -56,30 +56,38 @@ void addcampuses::on_addFile_clicked()
             QString startDist = QString::fromStdString(startingDist);
             QString endDist = QString::fromStdString(endingDist);
 
-            QSqlQuery query;
-            query.prepare("SELECT max(ID) from CAMPUSES"); // get the maximum id from the table
-            query.exec();
+            if (campusExists(startDist, endDist, distance))
+            {
+                QMessageBox::information(this, QObject::tr("System Message"), tr("Campus already exists. Cannot add campus"));
+                break;
+            }
+            else
+            {
+                QSqlQuery query;
+                query.prepare("SELECT max(ID) from CAMPUSES"); // get the maximum id from the table
+                query.exec();
 
-            // get the highest id from the bottom row of the table
-            if(query.next()) {
-                idNum =  query.value(0).toInt();
-                idNum++;
-                qDebug() << idNum;
+                // get the highest id from the bottom row of the table
+                if(query.next()) {
+                    idNum =  query.value(0).toInt();
+                    idNum++;
+                    qDebug() << idNum;
 
-                query.prepare("INSERT INTO CAMPUSES VALUES(:ID, :START, :STOP, :DIST)");
-                query.bindValue(":ID", idNum); // id is the id of the bottom row + 1
-                query.bindValue(":START", startDist);
-                query.bindValue(":STOP", endDist);
-                query.bindValue(":DIST", distance);
+                    query.prepare("INSERT INTO CAMPUSES VALUES(:ID, :START, :STOP, :DIST)");
+                    query.bindValue(":ID", idNum); // id is the id of the bottom row + 1
+                    query.bindValue(":START", startDist);
+                    query.bindValue(":STOP", endDist);
+                    query.bindValue(":DIST", distance);
 
-                success = query.exec();
+                    success = query.exec();
 
-                if(!success) {
-                    qDebug() << "addCampus error: " << query.lastError();
+                    if(!success) {
+                        qDebug() << "addCampus error: " << query.lastError();
+                    }
+
+                } else {
+                    qDebug() << "Error: addCampus did not get an ID value from the table.";
                 }
-
-            } else {
-                qDebug() << "Error: addCampus did not get an ID value from the table.";
             }
           }
          inFile.close();
@@ -89,4 +97,29 @@ void addcampuses::on_addFile_clicked()
 void addcampuses::on_AddCampus_clicked()
 {
     this->close();
+}
+
+bool addcampuses::campusExists(QString& start, QString& end, double& distance)
+{
+    QSqlQuery query;
+    bool success;
+    bool found;
+
+    query.prepare("SELECT EXISTS(SELECT 1 FROM CAMPUSES WHERE START=:START AND STOP=:STOP AND DIST=:DIST)");
+    query.bindValue(":START", start);
+    query.bindValue(":STOP", end);
+    query.bindValue(":DIST", distance);
+
+    success = query.exec();
+
+    if(!success) {
+          qDebug() << "campusExists error: " << query.lastError();
+          return false;
+    }
+
+    found = query.first();
+
+    //qDebug() << "found: " << found;
+
+    return found;
 }
